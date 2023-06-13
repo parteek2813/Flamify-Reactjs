8996b31ae029e1eea07e43d782ffd0da72c6f5a1
parteek kumar <digitalparteek@gmail.com>
2023-06-13 11:55:44 +0530

    Comepleted Basic Styling

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Messages = () => {
-  return <div className="messages">Messages</div>;
+  return (
+    <div className="messages owner">
+      <div className="messagesInfo">
+        <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=400"></img>
+        <span>Sent Now</span>
+      </div>
+      <div className="messagesContent">
+        <p>hello</p>
+        <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=400"></img>
+      </div>
+    </div>
+  );
 };
 
 export default Messages;
